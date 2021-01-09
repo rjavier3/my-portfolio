37869ffa3f675146f9e93c179b4eaa9c97e48172
Russell John Javier <teyjavier03@gmail.com>
2021-01-09 04:21:35 -0700

    tablet screen

--- js/main.js
+++ js/main.js
@@ -4,23 +4,27 @@ let navMenu = document.querySelector('header nav ul');
 
 
 //toggle nav Icon between burger and X
+function toggleNav(event){
+	navMenu.classList.toggle('hidden');
+	menuIcons[0].classList.toggle('hidden-icon');
+	menuIcons[1].classList.toggle('hidden-icon');
+}
+
 for(i = 0; i < menuIcons.length; i++) {
 	menuIcons[i].addEventListener('click', (event) =>{
-		navMenu.classList.toggle('hidden');
-		menuIcons[0].classList.toggle('hidden-icon');
-		menuIcons[1].classList.toggle('hidden-icon');
+		toggleNav();
 	});
 }
 
 //close nav when nav item clicked
 navMenu.addEventListener('click',(event)=>{
 	if(event.target.tagName.toLowerCase() === 'a'){
-		navMenu.classList.toggle('hidden');
+		toggleNav();
 	}
 	console.log(event.target);
 });
 
 //alert under construction
-document.addEventListener("DOMContentLoaded", (event)=>{
-	alert('HELLO, I am still working on this website and currently optimized for mobile screen only, it will get better soon!');
-});
+// document.addEventListener("DOMContentLoaded", (event)=>{
+// 	alert('HELLO, I am still working on this website and currently optimized for mobile screen only, it will get better soon!');
+// });
