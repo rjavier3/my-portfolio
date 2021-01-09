4ee402d936d801b282aae600d8a8faaf8633550e
Russell John Javier <teyjavier03@gmail.com>
2021-01-09 03:16:51 -0700

    animations

--- js/main.js
+++ js/main.js
@@ -1,20 +1,16 @@
 
-let menuIcon = document.querySelectorAll('.menu-icon');
-let menuBurger = menuIcon[0];
-let menuX = menuIcon[1];
-let navMenu = document.querySelector('header nav');
-let navItems = document.querySelectorAll('header nav li a');
-console.log(navItems);
+let menuIcons = document.querySelectorAll('.menu-icon');
+let navMenu = document.querySelector('header nav ul');
 
-//toggle nav Icon to burger or X
-function toggleNav(event){
-	navMenu.classList.toggle('hidden');
-	menuBurger.classList.toggle('hidden');
-	menuX.classList.toggle('hidden');
+
+//toggle nav Icon between burger and X
+for(i = 0; i < menuIcons.length; i++) {
+	menuIcons[i].addEventListener('click', (event) =>{
+		navMenu.classList.toggle('hidden');
+		menuIcons[0].classList.toggle('hidden-icon');
+		menuIcons[1].classList.toggle('hidden-icon');
+	});
 }
-
-menuBurger.addEventListener('click', toggleNav)
-menuX.addEventListener('click', toggleNav)
 
 //close nav when nav item clicked
 navMenu.addEventListener('click',(event)=>{
@@ -25,6 +21,6 @@ navMenu.addEventListener('click',(event)=>{
 });
 
 //alert under construction
-// document.addEventListener("DOMContentLoaded", (event)=>{
-// 	alert('HELLO, I am still working on this website and currently optimized for mobile screen only, it will get better soon!');
-// });
+document.addEventListener("DOMContentLoaded", (event)=>{
+	alert('HELLO, I am still working on this website and currently optimized for mobile screen only, it will get better soon!');
+});
